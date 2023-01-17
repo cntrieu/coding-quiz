be8c9028b17109436e09032c0ea6f111fa472201
Calvin Trieu <calvin.trieu@hotmail.com>
2023-01-17 01:45:41 -0500

    completed multiple functions and adjusted HTML & CSS

--- assets/script.js
+++ assets/script.js
@@ -11,37 +11,35 @@ var timesUp = document.querySelector("#times-up");
 var showScore = document.querySelector(".show-score");
 
 var score = 0;
+var currentQuestion, shuffledQuestions;
+
+var orderedList = document.querySelector("#ordered-list");
 
 var questionsArray = [
     {
         question: "which phase of propagation travels from the target node to the root node?",
-        answer: [{text: "A: Capturing", correct: false},
-        {text: "B: Propagation", correct: false},
-        {text: "C: Bubbling", correct: true},
-        {text: "D: Travel", correct: false}]
+        answers: ["A: Capturing", "B: Propagation", "C: Bubbling", "D: Travel"],
+        correct: "C: Bubbling"
     },
+    
     {
         question: "How can we retrieve an an HTML's attribute using JavaScript?",
-        answer: [{text: "A: retrieveAttribute", correct: false},
-        {text: "B: getAttribute", correct: true},
-        {text: "C: setAttribute", correct: false},
-        {text: "D: addAttribute", correct: false}]
+        answers: ["A: retrieveAttribute", "B: getAttribute", "C: setAttribute", "D: addAttribute"],
+        correct: "B: getAttribute"
+    },
+
+    {
+        question: "What methods can be used with timers?",
+        answers: ["setTimeout", "setInterval", "clearInterval", "All of the above"],
+        correct: "All of the above" 
     },
     {
-        question: "placeholder",
-        answer: [{text: "A: rAAAAAA", correct: false},
-        {text: "B: JJJJJJJ", correct: true},
-        {text: "C: setAttribute", correct: false},
-        {text: "D: addAttribute", correct: false}]
-    },
-    {
-        question: "PLACEHOLDING",
-        answer: [{text: "A: WDAWDS", correct: false},
-        {text: "B: WDASD", correct: true},
-        {text: "C: setAttribute", correct: false},
-        {text: "D: addAttribute", correct: false}]
+        question: "Which answer best represents appending a child element?",
+        answers: ["document.appendChild(document2)", "document(appendChild).document2", "appendChild(document[document2])", "All of the above"],
+        correct: "document.appendChild(document2)" 
     },
 ]
+
 
 // Randomizing the questions using Fisher-Yates algorithm
 var questionShuffle = function () {
@@ -54,8 +52,97 @@ var questionShuffle = function () {
         questionsArray[i] = questionsArray[j];
         questionsArray[j] = temp;
     }
-    console.log(questionsArray);
+    shuffledQuestions = questionsArray;
+    return shuffledQuestions;
 }
+
+var startQuiz = function () {
+    startPage.style.display = "none";
+    questionContainer.style.display = "block";
+    currentQuestion = 0;
+    timerStart();
+    questionShuffle();
+    nextQuestion();
+}
+
+var goNext = function () {
+    currentQuestion++;
+    nextQuestion();
+}
+
+
+var nextQuestion = function() {
+    displayQuestion(shuffledQuestions[currentQuestion])
+}
+
+var displayQuestion = function(question) {
+    questionsEl.innerHTML = question.question;
+
+    var answersContainer = document.createElement("div");
+    var answerli1 = document.createElement("button");
+    var answerli2 = document.createElement("button");
+    var answerli3 = document.createElement("button");
+    var answerli4 = document.createElement("button");
+
+    answerli1.innerHTML = questionsArray[currentQuestion].answers[0];
+    answerli2.innerHTML = questionsArray[currentQuestion].answers[1];
+    answerli3.innerHTML = questionsArray[currentQuestion].answers[2];
+    answerli4.innerHTML = questionsArray[currentQuestion].answers[3];
+
+    questionsEl.appendChild(answersContainer);
+    questionsEl.appendChild(answerli1);
+    questionsEl.appendChild(answerli2);
+    questionsEl.appendChild(answerli3);
+    questionsEl.appendChild(answerli4);
+
+    answerli1.addEventListener("click", answerCheck);
+    answerli2.addEventListener("click", answerCheck);
+    answerli3.addEventListener("click", answerCheck);
+    answerli4.addEventListener("click", answerCheck);
+}
+
+
+var answerCheck = function (e) {
+    var answerSelection = e.target;
+
+    if (answerSelection.textContent === questionsArray[currentQuestion].correct) {
+        score++;
+        displayCorrect();
+    } else {
+        timer.textContent -= 10;
+        displayINcorrect();
+    }
+
+    if(shuffledQuestions.length > currentQuestion + 1) {
+        goNext();
+    } else {
+        showResults();
+    }
+}
+
+var displayCorrect = function () {
+    var correctEl = document.createElement("h2");
+    correctEl.textContent = "Correct!";
+    var correctInterval = setInterval(function () {
+        correctEl.textContent = "";
+        clearInterval(correctInterval);
+    }, 500)
+
+    questionContainer.appendChild(correctEl);
+}
+
+var displayINcorrect = function () {
+    var inCorrectEl = document.createElement("h2");
+    inCorrectEl.textContent = "Wrong!";
+    var inCorrectInterval = setInterval(function () {
+        inCorrectEl.textContent = "";
+        clearInterval(inCorrectInterval);
+    }, 500)
+
+    questionContainer.appendChild(inCorrectEl);
+}
+
+
 
 var timerStart = function() {
     timer.textContent = 60;
@@ -67,7 +154,7 @@ var timerStart = function() {
             clearInterval(interval);
             timeOver();
          }
-    }, 10);
+    }, 1000);
 }
 
 var timeOver = function () {
@@ -75,12 +162,11 @@ var timeOver = function () {
     
 }
 
-startBtn.addEventListener("click", function() {
-    startPage.style.display = "none";
-    questionContainer.style.display = "block";
-    timerStart();
-    questionShuffle();
-})
+var showResults = function () {
+    // CREATE FUNCTION FOR THE END OF THE QUIZ
+}
+
+startBtn.addEventListener("click", startQuiz)
 
 // On window load, hide the other containers so only the start page appears
 window.onload = function () {
@@ -89,21 +175,30 @@ window.onload = function () {
 }
 
 
-//On user click of start -->
-    // - Begin Timer ;
-    // - highScore set to 0;
-    // - Manipulate HTML so section is hidden to reveal first question
-    //     - place questions in an object. Each question will have 3-4 answers with one true value and rest falsy values 
-    // - When question 1 is answered -->
-    //     - IF FALSE --> timer -10 seconds, change to next question
-    //     - IF TRUE --> score +10, change to next question
-    // - When timer = 0, display "Time's up!" 
-    //     - if currentScore > highScore, highScore = currentScore. 
-    //         - Store score of user initials in localStorage.setItem("")?? or smthin else
-    // When "View High Scores" is clicked on, localStorage.getItem() retrieves the users high score attempts
+// var displayQuestions = function () {
+//     var questionH2 = document.createElement("h2");
+//     var allAnswers = document.createElement("div");
+//     var answerli1 = document.createElement("button");
+//     var answerli2 = document.createElement("button");
+//     var answerli3 = document.createElement("button");
+//     var answerli4 = document.createElement("button");
+//     var answerSelector = document.querySelector("button");
 
+//     questionH2.innerHTML = questionsArray[currentQuestion].question;
 
-// localStorage.setItem("paragraph", content)
-// In the above code, what other line of code do we use to retrieve the value in the local storage?
-// a - localStorage.getItem("paragraph"), b - return localStorage(); 
+//         answerli1.innerHTML = questionsArray[currentQuestion].answers[0].text;
+//         answerli2.innerHTML = questionsArray[currentQuestion].answers[1].text;
+//         answerli3.innerHTML = questionsArray[currentQuestion].answers[2].text;
+//         answerli4.innerHTML = questionsArray[currentQuestion].answers[3].text;
+
+//     questionsEl.appendChild(questionH2);
+//     questionsEl.appendChild(allAnswers);
+//     allAnswers.appendChild(answerli1);
+//     allAnswers.appendChild(answerli2);
+//     allAnswers.appendChild(answerli3);
+//     allAnswers.appendChild(answerli4);
+
+//     allAnswers.setAttribute("style", "display: flex; flex-direction: column; align-items: flex-start;");
+
+// }
 
